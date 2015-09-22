33174d5118c8708c1fa311b6e2ebd850b5af573b
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2015-09-22 12:09:30 -0600

    Test a module

--- main.py
+++ main.py
@@ -1,57 +1,72 @@
 import sys
 import logging
 
-logger = logging.getLogger("module")
-logger.setLevel(logging.DEBUG)
-
-stream_handler = logging.StreamHandler(sys.stdout)
-stream_handler.setLevel(logging.INFO)
- 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-stream_handler.setFormatter(formatter)
- 
-logger.addHandler(stream_handler)
-
-# create a file handler
-handler = logging.FileHandler('module.log', 'w')
-handler.setLevel(logging.DEBUG)
-
-# create a logging format
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-
-# add the handlers to the logger
-logger.addHandler(handler)
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-stream_handler = logging.StreamHandler(sys.stdout)
+#logger = logging.getLogger("module.module")
+#logger.setLevel(logging.DEBUG)
+#
+##stream_handler = logging.StreamHandler(sys.stdout)
+##stream_handler.setLevel(logging.INFO)
+## 
+##formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+##stream_handler.setFormatter(formatter)
+## 
+##logger.addHandler(stream_handler)
+#
+## create a file handler
+#handler = logging.FileHandler('module.log', 'w')
+#handler.setLevel(logging.DEBUG)
+#
+## create a logging format
+#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#handler.setFormatter(formatter)
+#
+## add the handlers to the logger
+#logger.addHandler(handler)
+#
+#logger = logging.getLogger(__name__)
+#logger.setLevel(logging.DEBUG)
+#
+##stream_handler = logging.StreamHandler(sys.stdout)
+##stream_handler.setLevel(logging.DEBUG)
+##
+##formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+##stream_handler.setFormatter(formatter)
+##
+##logger.addHandler(stream_handler)
+#
+#
+## create a file handler
+#handler = logging.FileHandler("main.log", 'w')
+#handler.setLevel(logging.INFO)
+#
+## create a logging format
+#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#handler.setFormatter(formatter)
+#
+## add the handlers to the logger
+#logger.addHandler(handler)
+#
+#
+ch = logging.getLogger()
+ch.setLevel(logging.DEBUG)
+stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 stream_handler.setFormatter(formatter)
 
-logger.addHandler(stream_handler)
-
-
-# create a file handler
-handler = logging.FileHandler("main.log", 'w')
-handler.setLevel(logging.INFO)
-
-# create a logging format
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-
-# add the handlers to the logger
-logger.addHandler(handler)
-
-
-logger.info("Inside main")
-
+ch.addHandler(stream_handler)
 
 # Testing
+
 import module
 
 
-module.MOD()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+logger.info("Inside main")
+
+module.module.MOD()
+
+logger.info("End")
--- module/module.py
+++ module/module.py
@@ -5,6 +5,7 @@ logger = logging.getLogger(__name__)
 
 logger.info("initializing mod")
 
+
 class MOD(object):
     def __init__(self):
         logger.info("Created mod object\nAre you seeing this log in the stream?")
